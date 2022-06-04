f76e8412bb7435c381a5c9c894534b191d12a707
Ahmed Khaled Ismail Mohamed <72296578+ahmedkhaledismail@users.noreply.github.com>
2022-06-04 21:14:08 +0200

    Create timer_driver.c

---
+++ Embedded-C code/master microcontroller code/timer_driver.c
@@ -0,0 +1,25 @@
+
+#include "timer_driver.h"
+
+void timer0_initializeCTC(void)
+{
+	OCR0 = 78; //to achieve tick equal to 10 msec
+	
+	//Set the mode as Compare match
+	SET_BIT(TCCR0, WGM01);
+	CLR_BIT(TCCR0, WGM00);
+	
+	//set as clk/1024
+	SET_BIT(TCCR0, CS00);
+	CLR_BIT(TCCR0, CS01);
+	SET_BIT(TCCR0, CS02);
+	sei();
+	SET_BIT(TIMSK, OCIE0);
+}
+
+void timer0_stop(void)
+{
+	CLR_BIT(TCCR0, CS00);
+	CLR_BIT(TCCR0, CS01);
+	CLR_BIT(TCCR0, CS02);
+}
